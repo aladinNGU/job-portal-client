d158ed798b154532963d7ae5c4b798f56f3bd00c
alauddin <alauddinkmj@gmail.com>
2025-02-24 16:47:08 +0600

    posted job added

--- src/pages/AddJob/AddJob.jsx
+++ src/pages/AddJob/AddJob.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
 
 const AddJob = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
   const handleAddJob = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -9,9 +14,32 @@ const AddJob = () => {
     const initialData = Object.fromEntries(formData.entries());
     console.log("initial Data", initialData);
     const { min, max, currency, ...newJob } = initialData;
-    console.log('New Job1:', min, max, currency, newJob)
+    console.log("New Job1:", min, max, currency, newJob);
     newJob.salaryRange = { min, max, currency };
-    console.log('New Job2:', newJob)
+    newJob.requirements = newJob.requirements.split("\n");
+    newJob.responsibilities = newJob.responsibilities.split("\n");
+    console.log("New Job2:", newJob);
+
+    fetch("http://localhost:5000/jobs", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "Job has been added.",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          navigate("/myPostedJobs");
+        }
+      });
   };
 
   return (
@@ -50,10 +78,12 @@ const AddJob = () => {
             <label className="label">
               <span className="label-text">Job Type</span>
             </label>
-            <select name="jobType"className="select select-ghost w-full max-w-xs">
-              <option disabled selected>
-                Pick a job type
-              </option>
+            <select
+              name="jobType"
+              defaultValue="Pick a job type"
+              className="select select-ghost w-full max-w-xs"
+            >
+              <option disabled>Pick a job type</option>
               <option>Full-time</option>
               <option>Part-time</option>
               <option>Intern</option>
@@ -64,7 +94,10 @@ const AddJob = () => {
             <label className="label">
               <span className="label-text">Category</span>
             </label>
-            <select name="category"className="select select-ghost w-full max-w-xs">
+            <select
+              name="category"
+              className="select select-ghost w-full max-w-xs"
+            >
               <option disabled selected>
                 Pick a job category
               </option>
@@ -116,11 +149,10 @@ const AddJob = () => {
             </label>
             <select
               name="currency"
+              defaultValue="Pick a currency"
               className="select select-ghost w-full max-w-xs"
             >
-              <option disabled selected>
-                Pick a currency
-              </option>
+              <option disabled>Select a currency</option>
               <option>BDT</option>
               <option>USD</option>
               <option>EUR</option>
@@ -178,19 +210,6 @@ const AddJob = () => {
             required
           ></textarea>
         </div>
-        {/* Status */}
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Status</span>
-          </label>
-          <input
-            type="text"
-            name="status"
-            placeholder="Status"
-            className="input input-bordered"
-            required
-          />
-        </div>
         {/* HR Email */}
         <div className="form-control">
           <label className="label">
@@ -199,6 +218,7 @@ const AddJob = () => {
           <input
             type="email"
             name="hr_email"
+            defaultValue={user?.email}
             placeholder="Email"
             className="input input-bordered"
             required
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -8,6 +8,8 @@ import PrivateRoute from "./PrivateRoute";
 import JobApply from "../pages/JobApply/JobApply";
 import MyApplications from "../pages/MyApplications/MyApplications";
 import AddJob from "../pages/AddJob/AddJob";
+import MyPostedJobs from "../pages/MyPostedJobs/MyPostedJobs";
+import ViewApplications from "../pages/ViewApplications/ViewApplications";
 
 const router = createBrowserRouter([
   {
@@ -31,7 +33,29 @@ const router = createBrowserRouter([
       },
       {
         path: "addJob",
-        element: <AddJob></AddJob>,
+        element: (
+          <PrivateRoute>
+            <AddJob></AddJob>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "myPostedJobs",
+        element: (
+          <PrivateRoute>
+            <MyPostedJobs></MyPostedJobs>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "viewApplications/:job_id",
+        element: (
+          <PrivateRoute>
+            <ViewApplications></ViewApplications>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/job-applications/jobs/${params.job_id}`),
       },
       {
         path: "jobApply/:id",
